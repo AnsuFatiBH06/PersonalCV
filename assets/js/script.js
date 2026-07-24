87a57d7e8cbf804e5f9e4f42d06c7f4c60d0ed1c
AnsuFatiBH06 <195548364+AnsuFatiBH06@users.noreply.github.com>
2026-07-24 13:56:55 +0300

    adding a mode button, still no functionality

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,5 @@
+const body = document.body;
+
 // Displaying and Hiding information based on user clicking
 document.querySelectorAll('.clickableHeader').forEach(header => {
     header.addEventListener('click', () => {
@@ -13,3 +15,26 @@ document.querySelectorAll('.clickableHeader').forEach(header => {
         }
     });
 });
+
+document.querySelector('#themeSwitchButton').forEach(themeButton => {
+    themeButton.addEventListener('click', () => {
+        // toggle dark mode class
+        const buttonText = themeButton.querySelector('');
+
+        body.classList.toggle('darkMode');
+
+        if (body.classList.contains('darkMode')) {
+            buttonText.textContent = 'Toggle Light Mode';
+        } else {
+            buttonText.textContent = 'Toggle Dark Mode';
+        }
+    });
+});
+
+// // Save preference
+// localStorage.setItem('theme', body.classList.contains('darkMode') ? 'dark' : 'light');
+
+// // Load preference
+// if (localStorage.getItem('theme') === 'dark') {
+//     body.classList.add('darkMode');
+// }
