7e5c48867e36331fd76ff2af6fede82ff606ac4a
AnsuFatiBH06 <195548364+AnsuFatiBH06@users.noreply.github.com>
2026-09-11 00:40:55 +0300

    some more translations

--- assets/js/languageSwitch.js
+++ assets/js/languageSwitch.js
@@ -13,17 +13,30 @@ const translations = {
         // expandCollapseButton: 'Expand All<i class="fa fa-plus-square buttonToggleIcon"></i>',
         languageSwitchButton: 'Arabic<i class="fa fa-hand-o-left buttonToggleIcon"></i>',
         nameBig: "ABDULLA ABDULRAHIM",
+        
         feedbackTitle: "Your feedback on this website",
         feedbackDescription: "It's completely optional, however, I'd appreciate it if you take some time to share with me what can I do to improve this website!",
         feedbackLink: 'CLICK HERE TO SHARE<i class="fa fa-external-link-square buttonToggleIcon"></i>',
+        
         languagesHeader: 'Languages <span class="toggleIcon">(+)</span>',
         languagesArabic: '📜 Arabic<span>(Native)</span>',
         languagesEnglish: '📜 English<span>(IELTS UKVI 7.5)</span>',
+        
         skillsHeader: 'Skills <span class="toggleIcon">(+)</span>',
+        softSkills: "Soft Skills",
+        technicalSkills: "Technical Skills",
+        toolsUsed: "Tools Used",
+        
         educationHeader: 'Education <span class="toggleIcon">(+)</span>',
+        iknsEDU: '📍🏫 Ibn Khuldoon National School<span>2012 - 2024</span>',
+        strathEDU: '📍🏫 University of Strathclyde Bahrain<span>2024 - Ongoing</span>',
+
         workExperienceHeader: 'Work Experience <span class="toggleIcon">(+)</span>',
+        
         programsHeader: 'Programs <span class="toggleIcon">(+)</span>',
+        
         projectsHeader: 'Projects <span class="toggleIcon">(+)</span>',
+        
         certificationsHeader: 'Certifications <span class="toggleIcon">(+)</span>'
     }
     ,
@@ -33,16 +46,29 @@ const translations = {
         languageSwitchButton: 'English<i class="fa fa-hand-o-right buttonToggleIcon"></i>',
         nameBig: "عبدالله عبدالرحيم",
         feedbackTitle: "رأيكم نحو هذا الموقع",
+
         feedbackDescription: "الأمر اختياري تماماً، ومع ذلك، سأكون ممتناً لو خصصتم بعض الوقت لمشاركة أفكاركم معي حول ما يمكنني فعله لتحسين هذا الموقع!",
         feedbackLink: 'انقر هنا للمشاركة<i class="fa fa-external-link-square buttonToggleIcon"></i>',
+        
         languagesHeader: 'اللغات <span class="toggleIcon">(+)</span>',
         languagesArabic: '📜 العربية<span>(اللغة الأم)</span>',
         languagesEnglish: '📜 الإنجليزية<span>(7.5 في امتحان الآيلتس)</span>',
+        
         skillsHeader: 'المهارات <span class="toggleIcon">(+)</span>',
+        softSkills: "المهارات الشخصية",
+        technicalSkills: "المهارات التقنية",
+        toolsUsed: "الأدوات المستخدمة",
+
         educationHeader: 'التعليم <span class="toggleIcon">(+)</span>',
+        iknsEDU: '📍🏫 مدرسة ابن خلدون الوطنية <span>2012 - 2024</span>',
+        strathEDU: '📍🏫 جامعة ستراثكلايد البحرين <span>2024 - مستمر</span>',
+
         workExperienceHeader: 'الخبرة العملية <span class="toggleIcon">(+)</span>',
+        
         programsHeader: 'البرامج التعليمية <span class="toggleIcon">(+)</span>',
+        
         projectsHeader: 'المشاريع <span class="toggleIcon">(+)</span>',
+        
         certificationsHeader: 'الشهادات <span class="toggleIcon">(+)</span>'
     }
 };
